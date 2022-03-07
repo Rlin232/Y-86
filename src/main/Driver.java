a745cc7b6a7c2c716489af23a4795cc0f2bc42f8
Ryan Lin <rlin23205@gmail.com>
2022-03-07 13:27:34 -0800

    Parsing files into separate tokens, and storing in 2D String array

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,7 +1,51 @@
 package src.main;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        Path path = Paths.get("/Users/admin/Desktop/Ryan/OHS/2021-2022/Computer Systems/Y-86/input/in.txt");
+
+        String[][] tokens = tokenize(path);
+
+        for(int j = 0; j < tokens.length; j++) {
+            for(int k = 0; k < tokens[j].length; k++) {
+                System.out.print(tokens[j][k] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static String[][] tokenize(Path path) {
+        Supplier<Stream<String>> streamSupplier = () -> {
+            try {
+                return Files.lines(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        };
+
+        int numLines = (int) streamSupplier.get().count();
+
+        String[][] tokens = new String[numLines][];
+        
+        AtomicInteger i = new AtomicInteger();
+        Consumer<String> consumer = (String s) -> {
+            // First deal with comments and trim whitespaces
+            s = s.split("#")[0];
+            s = s.trim();
+
+            // Tokenize
+            tokens[i.getAndIncrement()] = s.split("\\s+");
+        }; 
+        streamSupplier.get().forEach(consumer);
+
+        return tokens;
     }
 }
