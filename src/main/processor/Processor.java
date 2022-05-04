b15aa399b85ca176753768b144f3f60c7f76fa49
Ryan Lin <rlin23205@gmail.com>
2022-05-04 11:38:18 -0700

    Beginnings of fetch method, format of output implemented so other methods can continue to be worked on

--- src/main/processor/Processor.java
+++ src/main/processor/Processor.java
@@ -4,6 +4,9 @@ public class Processor {
     String file;
     String pathOut;
 
+    int PC = 0; // Keeps track of which byte we're on
+    int index = 0; // Keeps track of which 4-bit word we're on
+
     public Processor(String file, String pathOut) {
         this.file = file;
         this.pathOut = pathOut;
@@ -11,10 +14,50 @@ public class Processor {
 
     public void process() {
         // Read through string until end, running through each step
-        
+
     }
     public int[] fetch() {
-        return new int[6];
+        int ifun = -1;
+        int valC = -1;
+        int valP = -1;
+        int valA = -1;
+        int valB = -1;
+        int valE = -1;
+        int rA = -1;
+        int rB = -1;
+        int instruction = Character.getNumericValue(file.charAt(index));
+        switch (instruction) {
+            case 0:
+                break;
+            case 1:
+                ifun = Character.getNumericValue(file.charAt(index + 1));
+                rA = Character.getNumericValue(file.charAt(index + 2));
+                rA = Character.getNumericValue(file.charAt(index + 3));
+                valP = PC + 2;
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+        }
+        int[] values = {ifun, valC, valP, valA, valB, valE, rA, rB};
+        return values;
     }
     public int[] decode() {
         return new int[3];
