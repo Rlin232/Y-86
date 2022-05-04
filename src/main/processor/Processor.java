05ca6b90937dcc33e739d73982c8a6ad02121f3b
Ryan Lin <rlin23205@gmail.com>
2022-05-04 12:04:10 -0700

    Fetch method should be done

--- src/main/processor/Processor.java
+++ src/main/processor/Processor.java
@@ -16,47 +16,89 @@ public class Processor {
         // Read through string until end, running through each step
 
     }
-    public int[] fetch() {
+
+    public int readWord() {
+        int word = Character.getNumericValue(file.charAt(index));
+        this.index++;
+        return word;
+    }
+    public long readEight() {
+        String value = file.substring(index, index+8);
+        index += 8;
+        return Long.parseLong(value);
+    }
+    public long[] fetch() {
         int ifun = -1;
-        int valC = -1;
+        long valC = -1;
         int valP = -1;
         int valA = -1;
         int valB = -1;
         int valE = -1;
         int rA = -1;
         int rB = -1;
-        int instruction = Character.getNumericValue(file.charAt(index));
+        int instruction = this.readWord();
+        
+        // The default operations
+        ifun = this.readWord();
+        
         switch (instruction) {
             case 0:
                 break;
             case 1:
-                ifun = Character.getNumericValue(file.charAt(index + 1));
-                rA = Character.getNumericValue(file.charAt(index + 2));
-                rA = Character.getNumericValue(file.charAt(index + 3));
-                valP = PC + 2;
                 break;
             case 2:
+                rA = readWord();
+                rB = readWord();
+                valP = PC + 2;
+                index += 4;
                 break;
             case 3:
+                rA = readWord();
+                rB = readWord();
+                valC = this.readEight();
+                valP = PC + 10;
                 break;
             case 4:
+                rA = readWord();
+                rB = readWord();
+                valC = this.readEight();
+                valP = PC + 10;
                 break;
             case 5:
+                rA = readWord();
+                rB = readWord();
+                valC = this.readEight();
+                valP = PC + 10;
                 break;
             case 6:
+                rA = readWord();
+                rB = readWord();
+                valP = PC + 2;
                 break;
             case 7:
+                valC = this.readEight();
+                valP = PC + 9;
                 break;
             case 8:
+                valC = this.readEight();
+                valP = PC + 9;
                 break;
             case 9:
+                valP = PC + 1;
                 break;
             case 10:
+                rA = readWord();
+                rB = readWord();
+                valP = PC + 2;
                 break;
             case 11:
+                rA = readWord();
+                rB = readWord();
+                valP = PC + 2;
                 break;
         }
-        int[] values = {ifun, valC, valP, valA, valB, valE, rA, rB};
+    
+        long[] values = {ifun, valC, valP, valA, valB, valE, rA, rB};
         return values;
     }
     public int[] decode() {
@@ -72,7 +114,8 @@ public class Processor {
     public void writeback() {
         // Update registers
     }
-    public int pcUpdate() {
-        return 0;
+    public void pcUpdate(int valP) {
+        PC = valP;
+        index = PC*2;
     }
 }
